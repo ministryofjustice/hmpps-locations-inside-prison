13c5cc162692f492ffed1c5cf066f26d69f18c0b
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2024-06-07 14:21:17 +0100

    MAP-1255 temporarily bodge test

--- integration_tests/pages/page.ts
+++ integration_tests/pages/page.ts
@@ -1,3 +1,6 @@
+import 'cypress-axe'
+import { Result } from 'axe-core'
+
 export type PageElement = Cypress.Chainable<JQuery>
 
 export default abstract class Page {
@@ -13,9 +16,35 @@ export default abstract class Page {
 
   checkOnPage(): void {
     cy.get('h1').contains(this.title)
+    if (!Cypress.env('SKIP_AXE')) {
+      cy.injectAxe()
+      cy.configureAxe({
+        rules: [
+          // Temporary rule whilst this issue is resolved https://github.com/w3c/aria/issues/1404
+          { id: 'aria-allowed-attr', reviewOnFail: true },
+          // Ignore the "All page content should be contained by landmarks", which conflicts with GOV.UK guidance (https://design-system.service.gov.uk/components/back-link/#how-it-works)
+          { id: 'region', reviewOnFail: true, selector: '.govuk-back-link' },
+        ],
+      })
+      cy.checkA11y(undefined, undefined, this.logAccessibilityViolations)
+    }
   }
 
   signOut = (): PageElement => cy.get('[data-qa=signOut]')
 
   manageDetails = (): PageElement => cy.get('[data-qa=manageDetails]')
+
+  logAccessibilityViolations(violations: Result[]): void {
+    cy.task('logAccessibilityViolationsSummary', `Accessibility violations detected: ${violations.length}`)
+
+    const violationData = violations.map(({ id, impact, description, nodes }) => ({
+      id,
+      impact,
+      description,
+      nodes: nodes.length,
+      nodeTargets: nodes.map(node => node.target).join(' - '),
+    }))
+
+    cy.task('logAccessibilityViolationsTable', violationData)
+  }
 }
