e8279f6d0a753a991281073b8f9f9678dba93a04
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2024-06-10 15:21:29 +0100

    MAP-1255 Temporarily enable a11y rule

--- integration_tests/pages/page.ts
+++ integration_tests/pages/page.ts
@@ -26,7 +26,7 @@ export default abstract class Page {
           // Temporary rule whilst this issue is resolved https://github.com/w3c/aria/issues/1404
           // { id: 'aria-allowed-attr', reviewOnFail: true },
           // Ignore the "All page content should be contained by landmarks", which conflicts with GOV.UK guidance (https://design-system.service.gov.uk/components/back-link/#how-it-works)
-          { id: 'region', reviewOnFail: true, selector: '.govuk-back-link' },
+          // { id: 'region', reviewOnFail: true, selector: '.govuk-back-link' },
         ],
       })
       cy.checkA11y(undefined, undefined, this.logAccessibilityViolations)
