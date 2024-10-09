6bb7fb7849d37f8fbffd075e462efd4980f1729a
Barry Gee <barry.gee@justice.gov.uk>
2024-10-09 13:20:04 +0100

    Remove commented code

--- cypress.config.ts
+++ cypress.config.ts
@@ -30,7 +30,6 @@ export default defineConfig({
     baseUrl: 'http://localhost:3007',
     excludeSpecPattern: '**/!(*.cy).ts',
     specPattern: 'integration_tests/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    // specPattern: 'integration_tests/e2e/changeTemporaryDeactivationsDetails/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'integration_tests/support/index.ts',
   },
 })
