d1066b9d54aa770f7e09d4d647dd98477024620f
jimbali <40831617+jimbali@users.noreply.github.com>
2024-10-11 14:46:00 +0100

    chore: Improved app insights telemetry (#141)
    Add username and Prison ID to the telemetry data, and ignore spammy paths
    
    MAP-1437

--- server/utils/azureAppInsights.ts
+++ server/utils/azureAppInsights.ts
@@ -1,4 +1,5 @@
 import {
+  Contracts,
   defaultClient,
   DistributedTracingModes,
   getCorrelationContext,
@@ -6,7 +7,13 @@ import {
   TelemetryClient,
 } from 'applicationinsights'
 import { RequestHandler } from 'express'
+import { EnvelopeTelemetry } from 'applicationinsights/out/Declarations/Contracts'
 import type { ApplicationInfo } from '../applicationInfo'
+
+export type ContextObject = {
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  [name: string]: any
+}
 
 export function initialiseAppInsights(): void {
   if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
@@ -36,6 +43,36 @@ export function buildAppInsightsClient(
     return defaultClient
   }
   return null
+}
+export function addUserDataToRequests(envelope: EnvelopeTelemetry, contextObjects: ContextObject) {
+  const isRequest = envelope.data.baseType === Contracts.TelemetryTypeString.Request
+  if (isRequest) {
+    const { username, activeCaseLoad } = contextObjects?.['http.ServerRequest']?.res?.locals?.user || {}
+    if (username) {
+      const { properties } = envelope.data.baseData
+      // eslint-disable-next-line no-param-reassign
+      envelope.data.baseData.properties = {
+        username,
+        activeCaseLoadId: activeCaseLoad?.id,
+        ...properties,
+      }
+    }
+  }
+  return true
+}
+
+export const ignorePathsProcessor = (envelope: EnvelopeTelemetry) => {
+  const prefixesToIgnore = ['GET /health', 'GET /info']
+
+  const isRequest = envelope.data.baseType === Contracts.TelemetryTypeString.Request
+  if (isRequest) {
+    const requestData = envelope.data.baseData
+    if (requestData instanceof Contracts.RequestData) {
+      const { name } = requestData
+      return prefixesToIgnore.every(prefix => !name.startsWith(prefix))
+    }
+  }
+  return true
 }
 
 export function appInsightsMiddleware(): RequestHandler {
