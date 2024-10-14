277c7adb4706db4bcda7135d86a6b9cdf1a0f211
Ashley Gyngell <ashley.gyngell@digital.justice.gov.uk>
2024-10-14 15:41:07 +0100

    MAP-1618: remove local name controller, route and view

---
+++ server/routes/removeLocalName/fields.ts
@@ -0,0 +1,14 @@
+const fields = {
+  localName: {
+    component: 'govukInput',
+    id: 'localName',
+    name: 'localName',
+    classes: 'govuk-input--width-3',
+    label: {
+      text: 'local name',
+      classes: 'govuk-fieldset__legend--m',
+    },
+  },
+}
+
+export default fields
