12f7cac4acbc5292cf8c2161f3e71791a972b0ea
Jim Bailey <jim.bailey@digital.justice.gov.uk>
2024-11-01 15:50:53 +0000

    chore: Send Google Analytics event for unhandled error
    MAP-1730

--- server/errorHandler.test.ts
+++ server/errorHandler.test.ts
@@ -1,6 +1,8 @@
-import type { Express } from 'express'
+import type { Express, Request, Response } from 'express'
 import request from 'supertest'
 import { appWithAllRoutes } from './routes/testutils/appSetup'
+import createErrorHandler from './errorHandler'
+import AnalyticsService from './services/analyticsService'
 
 let app: Express
 
@@ -33,5 +35,55 @@ describe('GET 404', () => {
         expect(res.text).toContain('Something went wrong. The error has been logged. Please try again')
         expect(res.text).not.toContain('NotFoundError: Not Found')
       })
+  })
+})
+
+describe('error handler', () => {
+  const errorHandler = createErrorHandler(true)
+  const analyticsService = new AnalyticsService(null) as jest.Mocked<AnalyticsService>
+  const req = {
+    originalUrl: '/location/7e570000-0000-0000-0000-000000000001/change-cell-capacity/confirm',
+    services: {
+      analyticsService,
+    },
+  } as unknown as Request
+  const res = {
+    locals: {
+      user: {
+        activeCaseload: {
+          id: 'TST',
+        },
+        username: 'NSCAMANDER',
+      },
+    },
+  } as Response
+
+  beforeEach(() => {
+    analyticsService.sendEvent = jest.fn()
+    res.render = jest.fn()
+    res.status = jest.fn()
+  })
+
+  it('should send an API error event to Google Analytics', () => {
+    const error: any = new Error('API error')
+    error.data = { errorCode: 117, status: 400 }
+
+    errorHandler(error, req, res, undefined)
+
+    expect(analyticsService.sendEvent).toHaveBeenCalledWith(req, 'unhandled_error', {
+      prison_id: 'TST',
+      error_code: 117,
+    })
+  })
+
+  it('should send an unknown error event to Google Analytics', () => {
+    const error: any = new Error('API error')
+
+    errorHandler(error, req, res, undefined)
+
+    expect(analyticsService.sendEvent).toHaveBeenCalledWith(req, 'unhandled_error', {
+      prison_id: 'TST',
+      error_code: undefined,
+    })
   })
 })
