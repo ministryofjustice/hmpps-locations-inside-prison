ce9a6fd6d47fdb3a3bcd5b31999521817820e2b2
Thource <me@thource.dev>
2025-01-21 14:36:46 +0000

    MAP-2059: Format Certification and Status in history

---
+++ server/utils/renderMacro.ts
@@ -0,0 +1,11 @@
+import nunjucks from 'nunjucks'
+
+export default function renderMacro(macroPath: string, macroName: string, params: object) {
+  const macroParams = JSON.stringify(params, null, 2)
+  const macroString = `
+      {%- from "${macroPath}" import ${macroName} -%}
+      {{- ${macroName}(${macroParams}) -}}
+    `
+
+  return nunjucks.renderString(macroString, undefined)
+}
