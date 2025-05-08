3a1f3b41be0f0fc7e5b321cebc1b0ff913baf265
Thource <me@thource.dev>
2025-05-08 12:10:39 +0100

    MAP-2383: Add max capacity permission checks to confirm cell capacity

--- server/controllers/changeCellCapacity/confirm.test.ts
+++ server/controllers/changeCellCapacity/confirm.test.ts
@@ -13,9 +13,14 @@ describe('ConfirmCellCapacity', () => {
   const authService = new AuthService(null) as jest.Mocked<AuthService>
   const locationsService = new LocationsService(null) as jest.Mocked<LocationsService>
   const analyticsService = new AnalyticsService(null) as jest.Mocked<AnalyticsService>
+  let permissions: { [permission: string]: boolean }
+  let sessionModel: { [key: string]: unknown }
 
   beforeEach(() => {
+    permissions = {}
+    sessionModel = { maxCapacity: '3', workingCapacity: '1' }
     req = {
+      canAccess: (permission: string) => permissions[permission],
       form: {
         values: {
           maxCapacity: '3',
@@ -31,11 +36,18 @@ describe('ConfirmCellCapacity', () => {
         referrerUrl: '/',
       },
       sessionModel: {
-        get: jest.fn((fieldName?: string) => ({ maxCapacity: '3', workingCapacity: '1' })[fieldName]),
+        get: jest.fn((fieldName?: string) => sessionModel[fieldName]),
+        set: jest.fn((fieldName?: string, value?: unknown) => {
+          sessionModel[fieldName] = value
+        }),
       },
     } as unknown as typeof req
     res = {
       locals: {
+        values: {
+          maxCapacity: 2,
+          workingCapacity: 2,
+        },
         location: {
           id: 'e07effb3-905a-4f6b-acdc-fafbb43a1ee2',
           capacity: {
@@ -63,27 +75,66 @@ describe('ConfirmCellCapacity', () => {
   })
 
   describe('locals', () => {
-    it('formats the change summary correctly', () => {
-      const result = controller.locals(req, res)
-      expect(result).toEqual({
-        backLink: '/location/e07effb3-905a-4f6b-acdc-fafbb43a1ee2/change-cell-capacity/change',
-        cancelLink: '/view-and-update-locations/TST/e07effb3-905a-4f6b-acdc-fafbb43a1ee2',
-        changeSummary: `You are decreasing the cell’s working capacity by 1.
+    describe('when the user has permission to change_max_capacity', () => {
+      beforeEach(() => {
+        permissions.change_max_capacity = true
+      })
+
+      it('formats the change summary correctly', () => {
+        const result = controller.locals(req, res)
+        expect(result).toEqual({
+          backLink: '/location/e07effb3-905a-4f6b-acdc-fafbb43a1ee2/change-cell-capacity/change',
+          cancelLink: '/view-and-update-locations/TST/e07effb3-905a-4f6b-acdc-fafbb43a1ee2',
+          changeSummary: `You are decreasing the cell’s working capacity by 1.
 <br/><br/>
 This will decrease the establishment’s working capacity from 20 to 19.
 <br/><br/>
 You are increasing the cell’s maximum capacity by 1.
 <br/><br/>
 This will increase the establishment’s maximum capacity from 30 to 31.`,
+        })
+      })
+    })
+
+    describe('when the user does not have permission to change_max_capacity', () => {
+      beforeEach(() => {
+        permissions.change_max_capacity = false
+      })
+
+      it('strips the max capacity from the change summary', () => {
+        const result = controller.locals(req, res)
+        expect(result).toEqual({
+          backLink: '/location/e07effb3-905a-4f6b-acdc-fafbb43a1ee2/change-cell-capacity/change',
+          cancelLink: '/view-and-update-locations/TST/e07effb3-905a-4f6b-acdc-fafbb43a1ee2',
+          changeSummary: `You are decreasing the cell’s working capacity by 1.
+<br/><br/>
+This will decrease the establishment’s working capacity from 20 to 19.`,
+        })
       })
     })
   })
 
   describe('saveValues', () => {
-    it('calls locationsService', async () => {
-      await controller.saveValues(req, res, next)
+    describe('when the user has permission to change_max_capacity', () => {
+      beforeEach(() => {
+        permissions.change_max_capacity = true
+      })
+      it('calls locationsService', async () => {
+        await controller.saveValues(req, res, next)
 
-      expect(locationsService.updateCapacity).toHaveBeenCalledWith('token', res.locals.location.id, 3, 1)
+        expect(locationsService.updateCapacity).toHaveBeenCalledWith('token', res.locals.location.id, 3, 1)
+      })
+    })
+
+    describe('when the user does not have permission to change_max_capacity', () => {
+      beforeEach(() => {
+        permissions.change_max_capacity = false
+      })
+      it('calls locationsService without the max capacity change', async () => {
+        await controller.saveValues(req, res, next)
+
+        expect(locationsService.updateCapacity).toHaveBeenCalledWith('token', res.locals.location.id, 2, 1)
+      })
     })
 
     it('sends an analytics event', async () => {
