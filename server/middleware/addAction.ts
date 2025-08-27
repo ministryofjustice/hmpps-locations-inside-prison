595260b4ea55bd059ce18ab990f736b41ab51f4d
Ashley Gyngell <ashley.gyngell@justice.gov.uk>
2025-08-27 13:37:45 +0100

    small tweaks

--- server/middleware/addAction.ts
+++ server/middleware/addAction.ts
@@ -2,7 +2,7 @@ import asyncMiddleware from './asyncMiddleware'
 
 export default function addAction(action: { text: string; href: string; class?: string }) {
   return asyncMiddleware((req, res, next) => {
-    const newAction = { ...action, class: action.class || 'govuk-button--secondary' }
+    const newAction = { class: 'govuk-button--secondary', ...action }
 
     res.locals.actions = res.locals.actions || []
     res.locals.actions.push(newAction)
