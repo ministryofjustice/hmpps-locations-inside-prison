301958937762fef7fa800e9fe9982b545a49c5bb
Ashley Gyngell <ashley.gyngell@justice.gov.uk>
2025-09-04 14:06:56 +0100

    cant edit an ACTIVE locations code, so throw error if location is not draft

--- server/routes/changeLocationCode/index.ts
+++ server/routes/changeLocationCode/index.ts
@@ -5,13 +5,23 @@ import fields from './fields'
 import protectRoute from '../../middleware/protectRoute'
 import populateDecoratedResidentialSummary from '../../middleware/populateDecoratedResidentialSummary'
 import populatePrisonAndLocationId from '../../middleware/populatePrisonAndLocationId'
+import asyncMiddleware from '../../middleware/asyncMiddleware'
 
 const router = express.Router({ mergeParams: true })
+
+const checkLocationIsDraft = asyncMiddleware(async (req, res, next) => {
+  if (res.locals.decoratedResidentialSummary.location.status !== 'DRAFT') {
+    throw new Error()
+  }
+
+  next()
+})
 
 router.use(
   protectRoute('change_location_code'),
   populatePrisonAndLocationId,
   populateDecoratedResidentialSummary,
+  checkLocationIsDraft,
   wizard(steps, fields, {
     name: 'change-location-code',
     templatePath: 'pages/changeLocationCode',
