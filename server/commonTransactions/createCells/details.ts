30b319c8aded0578b637a96524291989dae8cb15
Thource <adam.sharp@justice.gov.uk>
2025-11-25 09:07:37 +0000

    MAP-2561: Unset usedFor in create cells if accommodationType is not NA

--- server/commonTransactions/createCells/details.ts
+++ server/commonTransactions/createCells/details.ts
@@ -4,17 +4,21 @@ import BaseController from './baseController'
 
 export default class Details extends BaseController {
   override saveValues(req: FormWizard.Request, res: Response, next: NextFunction) {
+    const prefix = Object.keys(req.body)
+      .find(k => k.endsWith('cellsToCreate'))
+      .replace('cellsToCreate', '')
+
+    if (req.body[`${prefix}accommodationType`] !== 'NORMAL_ACCOMMODATION') {
+      req.sessionModel.unset(`${prefix}usedFor`)
+    }
+
     // If any of the values have changed, the user must go through the rest of the transaction (no longer editing)
-    if (req.isEditing) {
-      const prefix = Object.keys(req.body)
-        .find(k => k.endsWith('cellsToCreate'))
-        .replace('cellsToCreate', '')
-      if (
-        req.body[`${prefix}cellsToCreate`] !== req.sessionModel.get(`${prefix}cellsToCreate`) ||
-        req.body[`${prefix}accommodationType`] !== req.sessionModel.get(`${prefix}accommodationType`)
-      ) {
-        req.isEditing = false
-      }
+    if (
+      req.isEditing &&
+      (req.body[`${prefix}cellsToCreate`] !== req.sessionModel.get(`${prefix}cellsToCreate`) ||
+        req.body[`${prefix}accommodationType`] !== req.sessionModel.get(`${prefix}accommodationType`))
+    ) {
+      req.isEditing = false
     }
 
     super.saveValues(req, res, next)
