c2b2c12385cf68fb93d319c0d0cc066d354c45d1
Harriet Pulford <harriet.pulford@justice.gov.uk>
2026-07-31 08:46:05 +0100

    allowed-scripts version of @parcel/watcher updated

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -10,7 +10,7 @@ export default configureAllowedScripts({
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Needed by esbuild for watching files during development
-    'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
+    'node_modules/@parcel/watcher@2.6.0': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
     'node_modules/unrs-resolver@1.12.2': 'ALLOW',
   },
